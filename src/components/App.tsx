1b48ffd7be12cb8dd137da92bbd6463d954d8da5
neunyan <soundrabbitstd@gmail.com>
2020-02-25 16:07:46 +0900

    UIの追加

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,7 +7,9 @@ type Props = {}
 type State = {
     std_value: number,
     damage: number,
-    critical_number: number
+    critical_number: number,
+    accuracy: number,
+    evasion: number,
 }
 
 export class App extends React.Component<Props, State> {
@@ -17,7 +19,9 @@ export class App extends React.Component<Props, State> {
         this.state = {
             std_value: 0,
             damage: 0,
-            critical_number: 10
+            critical_number: 10,
+            accuracy: 0,
+            evasion: 0
         }
     }
 
@@ -49,25 +53,42 @@ export class App extends React.Component<Props, State> {
     }
 
     render(): JSX.Element | null {
+        const expected_damage = expected_value_of_damage_table(this.state.damage, this.state.std_value, this.state.critical_number);
         return (
             <div id="app">
-                <div className="input">
-                    <span>基準値</span>
-                    <Form.Control type="text" value={this.state.std_value.toString()} onInput={(e: React.FormEvent<HTMLInputElement>) => this.onInputStdValue(e.currentTarget.value)} />
+                <div>
+                    <div className="input">
+                        <span>基準値</span>
+                        <Form.Control type="text" value={this.state.std_value.toString()} onInput={(e: React.FormEvent<HTMLInputElement>) => this.onInputStdValue(e.currentTarget.value)} />
+                    </div>
+                    <div className="input">
+                        <span>威力</span>
+                        <Form.Control type="text" value={this.state.damage.toString()} onInput={(e: React.FormEvent<HTMLInputElement>) => this.onInputDamage(e.currentTarget.value)} />
+                    </div>
+                    <div className="input">
+                        <span>C値</span>
+                        <Form.Control type="text" value={this.state.critical_number.toString()} onInput={(e: React.FormEvent<HTMLInputElement>) => this.onInputCriticalNumber(e.currentTarget.value)} />
+                    </div>
                 </div>
                 <div className="input">
-                    <span>威力</span>
-                    <Form.Control type="text" value={this.state.damage.toString()} onInput={(e: React.FormEvent<HTMLInputElement>) => this.onInputDamage(e.currentTarget.value)} />
+                    <span>与ダメージの期待値</span>
+                    <span>{expected_damage.toFixed(2)}</span>
+                </div>
+                <div>
+                    <div className="input">
+                        <span>命中力</span>
+                        <Form.Control type="text" value={this.state.accuracy.toString()} />
+                    </div>
+                    <div className="input">
+                        <span>回避力</span>
+                        <Form.Control type="text" value={this.state.evasion.toString()} />
+                    </div>
                 </div>
                 <div className="input">
-                    <span>C値</span>
-                    <Form.Control type="text" value={this.state.critical_number.toString()} onInput={(e: React.FormEvent<HTMLInputElement>) => this.onInputCriticalNumber(e.currentTarget.value)} />
-                </div>
-                <div className="input">
-                    <span>期待値</span>
-                    <span>{expected_value_of_damage_table(this.state.damage, this.state.std_value, this.state.critical_number).toFixed(2)}</span>
+                    <span>DPSの期待値</span>
+                    <span>{expected_damage.toFixed(2)}</span>
                 </div>
             </div>
-        )
+        );
     }
 }
